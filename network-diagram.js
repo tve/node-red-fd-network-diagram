39ccf17fd79cc439e935fe56c68c211d8b0908d0
Thorsten von Eicken <tve@voneicken.com>
2022-03-25 10:47:42 -0700

    change API and data representation

--- network-diagram.js
+++ network-diagram.js
@@ -7,20 +7,80 @@ module.exports = function (RED) {
   function NetworkDiagram(config) {
     const fd = RED.nodes.getNode(config.fd) // get a handle onto FlexDash
     RED.nodes.createNode(this, config)
-    if (!fd) return // not much we can do, will have to wait for a FD node to be selected
+    if (!fd) return // not much we can do, wait for the next deploy...
+
+    // WARNING: this is a preliminary version of this node that updates all props of the widget
+    // at every change. This is not efficient, but more granular set/delete needs a bit more
+    // infrastrtucture work...
+    //
+    // Initialize the state of this node, specifically, we need to keep track of nodes, edges,
+    // zoom value, and a few more things so we can process add/remove operations locally and
+    // then update the FD state.
+    // The vis-network package expects arrays of nodes/edges, but we keep maps indexed
+    // by id to make insertion/deletion easier and to avoid duplicates.
+    this.state = {
+      // note: putting two nodes and one edge in for dev purposes, remove later...
+      nodes: {1: {id:1,name:'router'}, 2:{id:2,name:'laptop'}}, // ex: {id:1,name:'router'}
+      edges: {1001:{id:1001,from:1,to:2}}, // ex: {id:1001,from:1,to:2}
+      options: {},
+    }
     
-    // propagate this node's config to the FD widget
+    // initWidget ensures that a widget exists in FD and initializes its props with the
+    // second arg.
     // The third arg is the kind of widget to create, if it doesn't exist
+    Object.assign(config, this.state)
+    if (config.title === undefined || config.title === null) config.title = config.name
     fd.initWidget(this, config, 'NetworkDiagram')
 
-    // handle flow input messages, basically massage them a bit and update the FD widget
+    // handle flow input messages
     this.on("input", msg => {
-      // prepare update of widget params
-      const params = typeof msg.params === 'object' ? Object.assign({}, msg.params) : {}
-      // msg.payload is interpreted as setting the value
-      //if ('payload' in msg) params.payload_param = msg.payload
-      // send the params to the widget
-      fd.updateWidget(this, params)
+      const topic = msg.topic
+      const payload = msg.payload
+
+      switch(topic) {
+        case "set_network":
+            if(payload.nodes && !Array.isArray(payload.nodes)) {
+                throw "The payload.nodes should contain an array of nodes"
+            }
+
+            if(payload.edges && !Array.isArray(payload.edges)) {
+                throw "The payload.edges should contain an array of edges"
+            }
+            
+            this.state.nodes = {}
+            if (payload.nodes) {
+              this.state.nodes = Object.fromEntries(payload.nodes.map(n => [n.id, n]))
+            }
+            this.state.edges = {}
+            if (payload.edges) {
+              this.state.edges = Object.fromEntries(payload.edges.map(e => [e.id, e]))
+            }
+
+            if(payload.options && typeof payload.options === "object") {
+                Object.assign(this.state.options, payload.options)
+            }
+
+            fd.updateWidget(this, this.state)
+                       
+            break
+
+        case "add_nodes":
+            if(!payload) {
+                throw "The payload should contain one or more objects with node settings"
+            }
+            
+            if(!Array.isArray(payload)) payload = [payload]
+
+            const newNodes = Object.fromEntries(payload.map(n => [n.id, n]))
+            Object.assign(this.state.nodes, newNodes)
+
+            fd.updateWidget(this, this.state)
+            break
+
+        default:
+          throw "Unsupported command '" + topic + "' in topic"
+      }
+
     })
 
   }
--- widgets/network-diagram.vue
+++ widgets/network-diagram.vue
@@ -6,65 +6,72 @@
 </template>
 
 <script scoped>
-import { DataSet, Network } from 'vis-network/standalone/index.js'
+import { DataSet, Network } from "vis-network/standalone/index.js";
 
 export default {
-  name: 'NetworkDiagram',
+  name: "NetworkDiagram",
 
   help: `Computer network diagram.
 Displays a computer network diagram... Send it some JSON and it does magic...`,
 
   props: {
-    nodes: { type: Array, default() { return [
-      { id: 1, label: "Node 1" },
-      { id: 2, label: "Node 2" },
-      { id: 3, label: "Node 3" },
-      { id: 4, label: "Node 4" },
-      { id: 5, label: "Node 5" },
-    ] } },
-    edges: { type: Array, default() { return [
-      { from: 1, to: 3 },
-      { from: 1, to: 2 },
-      { from: 2, to: 4 },
-      { from: 2, to: 5 },
-      { from: 3, to: 3 },
-    ] } },
-    options: { type: Object, default() { return {
-      nodes: {
-        shape: 'box',
-        size: 10,
-        font: {
-          size: 12,
-          face: 'monospace',
+    nodes: {
+      type: Object,
+      tip: "map of nodes each with id: { id:.., label:.. }",
+      default() { return {
+        1: { id: 1, label: "Node 1" },
+        2: { id: 2, label: "Node 2" },
+        3: { id: 3, label: "Node 3" },
+      } },
+    },
+    edges: {
+      type: Object,
+      tip: "map of edges, each with id: { id:.., from:.., to:.. }",
+      default() { return {
+        1001: { id: 1001, from: 1, to: 3 },
+        1002: { id: 1002, from: 1, to: 2 }, 
+      } },
+    },
+    options: {
+      type: Object,
+      default() { return {
+        nodes: {
+          shape: "box",
+          size: 10,
+          font: { size: 12, face: "monospace", },
         },
-      },
-      edges: {
-        width: 2,
-        color: {
-          inherit: 'from',
+        edges: {
+          width: 2,
+          color: { inherit: "from", },
         },
-      },
-      physics: {
-        enabled: false,
-      },
-    } } },
-  },
-
-  computed: {
-    nodeDataSet() { return new DataSet(this.nodes) },
-    edgeDataSet() { return new DataSet(this.edges) },
-    dataSet() { return { nodes: this.nodeDataSet, edges: this.edgeDataSet } },
-  },
-
-  watch: { dataSet() { this.update() } },
-  mounted() { this.update() },
-  methods: {
-    update() {
-      console.log("Displaying network:", this.nodes, this.edges)
-      return new Network(this.$refs.netdiag, this.dataSet, this.options)
+        physics: {
+          enabled: false,
+        },
+      } },
     },
   },
 
+  data() { return {
+    network: null,
+  } },
+
+  computed: {
+    nodeDataSet() { return new DataSet(Object.values(this.nodes)) },
+    edgeDataSet() { return new DataSet(Object.values(this.edges)) },
+    dataSet() { return { nodes: this.nodeDataSet, edges: this.edgeDataSet } },
+  },
+
+  watch: {
+    dataSet() { this.update() },
+  },
+  mounted() { this.update() },
+  methods: {
+    update() {
+      console.log("Displaying network:", Object.keys(this.nodes), Object.keys(this.edges))
+      this.network = new Network(this.$refs.netdiag, this.dataSet, this.options)
+      this.$nextTick(() => this.network.redraw())
+    },
+  },
 }
 </script>
 
